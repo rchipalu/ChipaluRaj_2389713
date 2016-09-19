2c26b64663809440426ff6e92eb32ec2fc3d42dc
rchipalu <raj.chipalu@gmail.com>
2016-09-19 09:41:05 -0700

    Add files via upload

---
+++ Homework/Savitch/Savitch_9thEd_Chap1_ProgProj_Prob4/main.cpp
@@ -0,0 +1,43 @@
+/* 
+   File:  Savitch Chapter 1 Program Projects Problem 4
+   Author: Raj Chipalu
+   Created on September 18 , 2016, 9:41 AM
+   Purpose: Write a program that allows the user to enter a time in seconds and then
+outputs how far an object would drop if it is in freefall for that length of
+time.
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+#include <cmath>
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+const float GRAVITY=32.174;//Acceleration
+
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    float time;  //Time as input in seconds
+    float dftFell; //Distance in feet
+    
+    //Input values
+    cout<<"Imput the time in free fall in seconds"<<endl;
+    cin>>time;
+    
+    //Process values -> Map inputs to Outputs
+    dftFell = GRAVITY*time*time/2;
+            
+    //Display Output
+    cout<<"Gravity = "<<GRAVITY<<" ft/sec^2"<<endl;
+    cout<<"Time fell = "<<time<<" seconds"<<endl;
+    cout<<"Distance traveled = "<<dftFell<<" ft"<<endl;
+
+    //Exit Program
+    return 0;
+}
