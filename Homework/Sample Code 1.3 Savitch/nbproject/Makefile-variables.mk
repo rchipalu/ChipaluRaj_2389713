62632e33309f4bc9a5fcf52445b91d633188f683
Raj Chipalu <raj.chipalu@gmail.com>
2016-09-20 21:51:14 -0700

    done

---
+++ Homework/Sample Code 1.3 Savitch/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=sample_code_1.3_savitch
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/sample_code_1.3_savitch
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=samplecode1.3savitch.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/samplecode1.3savitch.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=sample_code_1.3_savitch
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/sample_code_1.3_savitch
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=samplecode1.3savitch.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/samplecode1.3savitch.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
